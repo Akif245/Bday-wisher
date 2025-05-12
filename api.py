97941799b572e6b4ec350a25087170ad17306144
Akif245 <abdulakif44570@gmail.com>
2025-05-12 13:37:21 +0530

    API request

--- api.py
+++ api.py
@@ -1,8 +1,23 @@
 import requests
 
-response=requests.get("https://annafifoundation.com/")
+response=requests.get(url="https://annafifoundation.com/")
+response.raise_for_status()
+
+data=response.json()
+
+longitude=data["iss_position"]["longitude"]
+latitude=data["iss_position"]["latitude"]
+
+iss_position=(longitude,longitude)
+print(iss_position)
+
+
+
+
+
+
 # print(response)
-if response.status_code!=200:
-    print("Error")
-else:
-    print("Success!")
+# if response.status_code!=200:
+#     print("Error")
+# else:
+#     print("Success!")
